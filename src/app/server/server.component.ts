d1af7d075782da8b739e7f70780ef5772d66e27f
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2023-04-15 17:49:17 +0530

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,13 @@ export class ServerComponent {
   firstName: string = 'Vishal';
   lastName: string = 'Kumar';
   city: string = 'Jalandhar City';
+  disableBtn: boolean = true;
+
+  constructor() {
+    setInterval(() => {
+      this.disableBtn = !this.disableBtn;
+    }, 500);
+  }
 
   whoAreYou() {
     return `My name is ${this.firstName} ${this.lastName} from ${this.city}`;
