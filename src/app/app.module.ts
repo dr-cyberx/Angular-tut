486051be474862a4b0642ab98cec31a232fe48cd
Vishal <74290663+dr-cyberx@users.noreply.github.com>
2023-04-15 16:55:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
+import { SuccessAlert } from './successAlert/successAlert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ErrorAlertComponent, SuccessAlert],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './successAlert.component.html',
+  styleUrls: ['./successAlert.component.css'],
+  selector: 'alert-success',
+})
+export class SuccessAlert {}
